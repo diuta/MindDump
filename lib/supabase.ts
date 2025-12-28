7e6a92f276f4a51d23d33577d241cf3bcc40038a
diuta <dimasptra@gmail.com>
2025-12-28 19:42:29 +0700

    bugfix authentication

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,4 +1,4 @@
-import { createClient } from "@supabase/supabase-js";
+import { createBrowserClient } from '@supabase/ssr'
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
 const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
@@ -7,4 +7,5 @@ if (!supabaseUrl || !supabaseKey) {
   throw new Error('Missing Supabase URL or Anon Key. Check your .env.local file!')
 }
 
-export const supabase = createClient(supabaseUrl, supabaseKey);
+// This client will now automatically sync sessions with cookies
+export const supabase = createBrowserClient(supabaseUrl, supabaseKey);
